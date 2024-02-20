eb7dc3bde4594199003428efb923b77c6512d30e
soyshan <shanbu131@gmail.com>
2024-02-20 20:22:00 +0100

    delete auth component

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,6 @@ import CompCreateBlog from './components/blog/CreateBlog';
 import CompEditBlog from './components/blog/EditBlog';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import AuthPage from './components/AuthPage';
 import CompShowBlogsAsCards from './components/RecipesCard/CompShowBlogAsCards';
 import BlogDetailPage from './components/BlogUnico/BlogDetailPage';
 import ContactForm from './components/ContactForm/ContactForm';
@@ -23,7 +22,6 @@ function App() {
           <Route path ='/create' element ={<CompCreateBlog/>}/>
           <Route path ='/edit/:id' element ={<CompEditBlog/>}/>
           <Route path ='/recipes' element ={<CompShowBlogsAsCards/>}/>
-          <Route path ='/auth' element ={<AuthPage/>}/>
           <Route path='/blogs/:id' element={<BlogDetailPage/>} /> 
           <Route path ='/contact' element ={<ContactForm/>}/>
           
