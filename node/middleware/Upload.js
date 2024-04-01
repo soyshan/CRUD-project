006d3f68de71860437b4b31c9f96ce56abb816c7
soyshan <shanbu131@gmail.com>
2024-04-01 19:51:41 +0200

    modified:   front/src/components/blog/CreateBlog.js modified:   front/src/components/footer/footerStyle.css 	modified:   front/src/tests/ContactForm.test.js 	modified:   front/src/tests/Navbar.test.js 	modified:   node/middleware/Upload.js 	new file:   node/uploads/1709205199389.png

--- node/middleware/Upload.js
+++ node/middleware/Upload.js
@@ -9,9 +9,14 @@ const storage = multer.diskStorage({
         cb(null, `${Date.now()}.${ext}`);
     }
 });
+//cb se utiliza para especificar la carpeta de destino donde se guardarán los archivos cargados
+//null como primer argumento (indicando que no ha ocurrido un error) y 'uploads/' 
+//como segundo argumento, que es la carpeta donde se guardarán los archivos.
+
 
 const upload = multer({ storage:storage});
 
 export const uploadImage = upload.single('image_url');
 
 
+//middleware  manejar la carga de archivos utilizando la biblioteca Multer
