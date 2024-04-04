c127f08e7b134952a94fb5d8ace860c033f662c2
soyshan <shanbu131@gmail.com>
2024-04-04 23:03:59 +0200

    create usersmodel and users controller

--- node/models/BlogModel.js
+++ node/models/BlogModel.js
@@ -2,7 +2,7 @@
 import db from "../database/db.js"
 //importamos sequelize
 import {DataTypes} from "sequelize";
-
+import UsersModel from "./UsersModel.js";
 //definimos nuestra conexión, y usamos el nombre de la tabla: recetas
 const BlogModel = db.define('recetas',{
     title: { type: DataTypes.STRING},
@@ -11,6 +11,8 @@ const BlogModel = db.define('recetas',{
     image_url: { type: DataTypes.STRING } 
 
 })
+// Establecer la relación entre la tabla de recetas  y la de usuarios
+BlogModel.belongsTo(UsersModel, { foreignKey: 'user_id' });
 
                     
 export default BlogModel
